6e7d9392dd5021596e61e4319adb53875ed2c1fd
Nsamiksha <nannawaresami@gmail.com>
2019-06-05 19:08:21 +0530

    f commit

---
+++ ToBinary.java
@@ -0,0 +1,14 @@
+package algorith;
+
+import Utility.util;
+
+public class ToBinary {
+	public static void main(String[] args) 
+    {
+        String binary = util.tobinary();
+        System.out.println("Decimal to Binary convertion is :" + binary);
+        
+        //int decimal = util.todecimal(binary);
+        //System.out.println("Binary to Decimal convertion is :" + decimal);
+}
+}
